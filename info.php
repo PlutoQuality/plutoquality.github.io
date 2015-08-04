348f9ce3cfe8c2a30fbba7de40ffeb6c46fda319
PlutoQuality <plutoquality@gmail.com>
2015-08-04 14:00:39 -0500

    START

---
+++ info.php
@@ -0,0 +1,22 @@
+<html>
+  <title> info </title>
+  <head>
+    <meta name="descrition" content="Free Birthday Webpage">
+    <meta name="author" content="Devon Schimming">
+    <meta http-equiv="refresh" content="30">
+  </head>
+  <style>
+    div.thing {
+      margin-left: auto;
+      margin-right: auto;
+      width: 75%;
+    }
+  </style>
+  <body>
+    <div class="thing">
+      <P>This rather narrow block of text is centered. Note that the lines inside the block are not centered (they are left-aligned), unlike in the earlier example.</p>
+    </div>
+
+  </div>
+  </body>
+</html>
---
+++ swf/index123.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<style type="text/css">
+body {
+    margin: 0 auto 20px;
+    padding: 0;
+    background: #333;
+    text-align: center;
+}
+h1.title{
+	background: #fff;
+	margin: 0;
+	padding: 0;
+}
+li {
+	display: inline;
+}
+ul.navBar {
+	background: #fff;
+	outline: solid;
+	list-style-type: none;
+	margin: 0;
+	padding: 0;
+}
+ul.Folders {
+	background: #fff;
+	list-style-type: none;
+	margin: 0;
+	padding: 0;
+}
+a {
+	text-align: center-left;
+	padding-left: 10px;
+}
+#Folders {
+	text-align: left;
+	display: block;
+	background: #fff;
+	width: 140px;
+	height: 61px;
+	padding: 0px;
+	outline: solid;
+	outline-color: #000;
+}
+
+</style>
+<head><title>NonSix/swf/</title></head>
+<body>
+	<h2></h2>
+	<h1 class="title">/swf/</h1>
+	<ul class="navBar">
+		<li><a href="../home">Home</a></li>
+		<li><a href="../contact">Contact</a></li>
+	</ul>
+	<ul>
+		<li><a id="Folders" href="./anime"><img  src="./Anime.png"></a></li>
+		<li><a id="Folders" href="./games"><img src="./games.png"></a></li>
+		<li><a id="Folders" href="./heliakbar">ALLAH AKBAR</a></li>
+		<li><a id="Folders" href="./loops">Loops</a></li>
+		<li><a id="Folders" href="./songs">Songs</a></li>
+		<li><a id="Folders" href="./weed">WEED</a></li>
+		<li><a id="Folders" href="./mlg">MLG</a></li>
+	</ul>
+	<p class="folders">
+	</p>
+</body>
+</html> 
